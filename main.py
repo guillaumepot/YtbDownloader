4ddab27967bbacfd1617b1c70932485c6b24c58d
guillaumepot <guillaume.pot@outlook.com>
2025-05-13 11:26:04 +0200

    Updated downloader class and main script

--- main.py
+++ main.py
@@ -1,11 +1,66 @@
-from src.LoggerManager import LoggerManager
+import os
 
+from src.LoggerManager import LoggerManager
+from src.YtbVideoDownloader import YtbVideoDownloader
 
 
 def main():
-    console_logger = LoggerManager.configure_logger(name='script', verbose=True)
+    # Set Logger
+    logger = LoggerManager.configure_logger(name='script', verbose=True)
 
-    console_logger.debug('Hello, World!')
+    # Get URL file path & set downloader
+    print("Download video from file or playlist ?")
+    print("1. File")
+    print("2. Playlist")
+
+    while True:
+        try:
+            choice = int(input("Choice: "))
+            if choice in [1, 2]:
+                video_source = 'file' if choice == 1 else 'playlist'
+                break
+            else:
+                print("Invalid choice, please choose 1 or 2.")
+        except ValueError:
+            print("Invalid input, please enter a number (1 or 2).")
+
+
+
+
+    url_path = input("Enter the path or url to the video URL file: ")
+    if video_source == 'file' and not os.path.exists(video_source):
+        logger.error(f"File {video_source} does not exist.")
+        exit(1)
+
+
+
+    print("Download video or audio only?")
+    print("1. Video")
+    print("2. Audio")
+
+    while True:
+        try:
+            choice = int(input("Choice: "))
+            if choice in [1, 2]:
+                output_dir = './storage/videos' if choice == 1 else './storage/audios'
+                type = 'video' if choice == 1 else 'audio'
+                break
+            else:
+                print("Invalid choice, please choose 1 or 2.")
+        except ValueError:
+            print("Invalid input, please enter a number (1 or 2).")
+
+
+
+    downloader = YtbVideoDownloader(
+        video_source = video_source,
+        url_path = url_path,
+        output_dir = output_dir,
+        type = type
+        )
+
+    downloader.download_videos()
+
 
 
 if __name__ == "__main__":
--- src/YtbVideoDownloader.py
+++ src/YtbVideoDownloader.py
@@ -1,8 +1,148 @@
 
+import os
+from pytubefix import YouTube, Playlist
+from pytubefix.cli import on_progress
 
 
+from src.LoggerManager import LoggerManager
 
 
 class YtbVideoDownloader():
-    def __init__(self):
-        pass
+    def __init__(self, video_source: str, url_path:str, output_dir:str, type:str):
+        if type not in ['video', 'audio']:
+            raise ValueError("Invalid type. Must be 'video' or 'audio'.")
+        self.video_source = video_source
+        self.url_path = url_path
+        self.output_dir = output_dir
+        self.type = type
+        self.extension = '.mp4' if self.type == 'video' else '.mp3'
+        self.logger = LoggerManager.configure_logger(name='YtbVideoDownloader', log_dir='./logs/', verbose=True)
+
+
+        # Create the output directories if they do not exist
+        if not os.path.exists(self.output_dir):
+            self.logger.info(f"Creating output video directory: {self.output_dir}")
+            os.makedirs(self.output_dir, exist_ok=True)
+        if not os.path.exists(self.output_dir):
+            self.logger.info(f"Creating output video directory: {self.output_dir}")
+            os.makedirs(self.output_dir, exist_ok=True)
+
+
+    def control_link(self, link:str) -> bool:
+        """
+        Check if the provided link is a valid YouTube URL.
+
+        Args:
+            link (str): The YouTube URL to be checked.
+
+        Returns:
+            bool: True if the link is valid, False otherwise.
+        """
+        if not link.startswith("https://www.youtube.com/") and not link.startswith("https://youtu.be/"):
+            return False
+        return True
+    
+
+    def check_duplicated_links(self, link:str) -> bool:
+        """
+        Check if the provided link already exists in the video URL file.
+
+        Args:
+            link (str): The YouTube URL to be checked.
+
+        Returns:
+            bool: True if the link is duplicated, False otherwise.
+        """
+        with open(self.url_path, 'r') as file:
+            for line in file:
+                if line.strip() == link:
+                    
+                    return True
+        return False
+
+
+    def get_youtube_video_data(self, link:str):
+        """
+        Retrieves information about a YouTube video.
+
+        Args:
+            link (str): The URL of the YouTube video.
+
+        Returns:
+            tuple: A tuple containing a dictionary with the video information and the video stream.
+
+        Raises:
+            SomeException: Description of the exception raised.
+
+        """
+        video = YouTube(link, on_progress_callback = on_progress)
+        stream = video.streams.get_highest_resolution() if self.type == 'video' else video.streams.get_audio_only()
+
+
+        video_data = {
+            "title": video.title,
+            "duration": video.length,
+            "size": stream.filesize / 1024 / 1024,
+            "thumbnail": video.thumbnail_url,
+            "stream": stream
+        }
+
+        return video_data    
+
+    
+
+    def download_videos(self) -> None:
+        """
+        Download videos from the provided YouTube URLs.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+
+        # Load links
+        if self.video_source == 'file':
+            bad_links:list[str] = []
+            links:list[str] = []
+            duplicated_links_counter = 0
+            with open(self.url_path, 'r') as file:
+                for line in file:
+                    link = line.strip()
+                    if not self.control_link(link):
+                        self.logger.error(f'Bad link: "{link}"')
+                        bad_links.append(link)
+                    elif link in links:
+                        self.logger.warning(f'Duplicated link found: "{link}", skipping.')
+                        duplicated_links_counter +=1
+                    else:
+                        links.append(link)
+
+            # Download links
+            for link in links:
+                self.logger.info(f"Retrieving video data from: {link}")
+                video_data = self.get_youtube_video_data(link)
+                self.logger.info(f"Dowloading video: {video_data['title']}, duration: {video_data['duration']} seconds, size: {video_data['size']} MB")
+                try:
+                    video_data['stream'].download(output_path=self.output_dir, filename=video_data['title'] + self.extension)
+                    self.logger.info("Video downloaded successfully")
+                except Exception as e:
+                    self.logger.error(f"Error downloading video: {video_data['title']}, Error: {e}")
+
+            self.logger.info("Finished downloading files.")
+            self.logger.warning(f"Skipped {duplicated_links_counter} duplicated links")
+            self.logger.warning(f"Bad links: {'\n'.join(bad_links)}")
+
+
+        elif self.video_source == 'playlist':
+            pl = Playlist(self.url_path)
+            for video in pl.videos:
+                self.logger.info(f"Retrieving video data from: {video.watch_url}")
+                video_data = self.get_youtube_video_data(video.watch_url)
+                self.logger.info(f"Dowloading video: {video_data['title']}, duration: {video_data['duration']} seconds, size: {video_data['size']} MB")
+                try:
+                    video_data['stream'].download(output_path=self.output_dir, filename=video_data['title'] + self.extension)
+                    self.logger.info("Video downloaded successfully")
+                except Exception as e:
+                    self.logger.error(f"Error downloading video: {video_data['title']}, Error: {e}")
